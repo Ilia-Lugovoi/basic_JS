a33e7cd70f9498cd3c66439e741462012012a65b
Ilya <ilya.lugovoy012@gmail.com>
2023-12-10 11:00:23 +0300

    mor test

--- my_first_site/site/index.php
+++ my_first_site/site/index.php
@@ -9,28 +9,28 @@
 	<body>
 
 		<div class="content">
-		<?php
+			<?php
 			include "menu.php";
-		?>
+			?>
 
-		<h1>Личный сайт студента GeekBrains</h1>
+			<h1>Личный сайт студента GeekBrains</h1>
 
-		<div class="center">
-			<img src="img/DSC08931.JPG">
-		<div class="box_text">
-			<p><b>Добрый день</b>. Меня зовут <i>Ilia Lugovoi</i>. Предлагаю вашему внимаю мой сайт, на котором вы сможете сыграть в игры</p>
+			<div class="center">
+				<img src="img/DSC08931.JPG">
+				<div class="box_text">
+					<p><b>Добрый день</b>. Меня зовут <i>Ilia Lugovoi</i>. Предлагаю вашему внимаю мой сайт, на котором вы сможете сыграть в игры</p>
 
-			<p>В этом мне помог IT-портал <a href="https://geekbrains.ru">GeekBrains</a></p>
+					<p>В этом мне помог IT-портал <a href="https://geekbrains.ru">GeekBrains</a></p>
 
-			<p>Представляю Вашему вниманию: <br><br>
-				<a href="index.php">Главная</a>
-				<a href="puzzle.php">Загадки</a>
-				<a href="guess.php">Угадайка</a> <br>
-				<a href="guess2.php">Угадайка на двоих</a>
-				<a href="password_generator.php">Генератор паролей</a>
-			</p>
-		</div>
-		</div>
+					<p>Представляю Вашему вниманию: <br><br>
+						<a href="index.php">Главная</a>
+						<a href="puzzle.php">Загадки</a>
+						<a href="guess.php">Угадайка</a> <br>
+						<a href="guess2.php">Угадайка на двоих</a>
+						<a href="password_generator.php">Генератор паролей</a>
+					</p>
+				</div>
+			</div>
 		</div>
 
 		<div class="footer">
